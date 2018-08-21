840eeb5077f82d472765d8804adf3049194346b7
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-21 14:16:15 +0100

    InittiCommitti

---
+++ controller.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/contrib/all' if development?
+require_relative('./models/calculator')
+also_reload('./models/*')
+
+get '/add/:num1/:num2' do
+  calculator = Calculator.new(params[:num1].to_i, params[:num2].to_i)
+  @calculation = calculator.add()
+  erb(:result)
+  # return "#{params[:num1]} + #{params[:num2]} = #{@calculation}"
+end
+
+get '/subtract/:num1/:num2' do
+  calculator = Calculator.new(params[:num1].to_i, params[:num2].to_i)
+  @calculation = calculator.subtract()
+  # return "#{params[:num1]} - #{params[:num2]} = #{@calculation}"
+  erb(:result)
+end
+
+get '/multiply/:num1/:num2' do
+  calculator = Calculator.new(params[:num1].to_i, params[:num2].to_i)
+  @calculation = calculator.multiply()
+  # return "#{params[:num1]} - #{params[:num2]} = #{@calculation}"
+  erb(:result)
+end
+
+get '/divide/:num1/:num2' do
+  calculator = Calculator.new(params[:num1].to_i, params[:num2].to_i)
+  @calculation = calculator.divide()
+  # return "#{params[:num1]} - #{params[:num2]} = #{@calculation}"
+  erb(:result)
+end
